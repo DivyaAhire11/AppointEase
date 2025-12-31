debfd5def842b2002d7498eab834f195c39dbe2e
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-31 18:27:45 +0530

    Footer Added

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,1 +1,33 @@
-<h1> Footer </h1>
+<footer class="footer">
+
+    <div class="footer-container">
+
+        <div class="footer-box">
+            <h3>AppointCare</h3>
+            <p>
+                AppointCare helps you book doctor appointments easily and quickly.
+                Trusted by patients and healthcare professionals.
+            </p>
+        </div>
+
+        <div class="footer-box">
+            <h4>Quick Links</h4>
+            <ul>
+                <li ><a href="#">Home</a></li>
+                <li ><a href="#">Our Doctors</a></li>
+                <li ><a href="#">Book Appointment</a></li>
+                <li ><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+
+         <div class="footer-box">
+            <h4>Contact</h4>
+            <p>Email: support@appointcare.com</p>
+            <p>Phone: +91 xxxxxxxxxxxx</p>
+            <p>Location: India</p>
+        </div>
+    </div>
+     <div class="footer-bottom">
+        <p>© 2025 AppointCare. All Rights Reserved.</p>
+    </div>
+</footer>
