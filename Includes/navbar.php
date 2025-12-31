21d670a4407800bf970fa2da1e90a29b723fb63a
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-31 12:51:17 +0530

    NavBar create

---
+++ Includes/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+      <div class="logo">Appoint<span>System</span></div>
+     <ul class="menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Pages/services.php">Services</a></li>
+        <li><a href="Pages/doctors.php">Doctors</a></li>
+        <li><a href="Pages/login.php">Login</a></li>
+        <li><a href="Pages/bookAppoint.php" class="btn-primary">Book Appointment</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appoint Booking System</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="page1">
+        <?php include 'includes/navbar.php'; ?>
+    </div>
+
+    <?php include 'includes/homePage1.php'; ?>
+    <?php include 'includes/homePage2.php'; ?>
+    <?php include 'includes/footer.php'; ?>
+</body>
+
+</html>
