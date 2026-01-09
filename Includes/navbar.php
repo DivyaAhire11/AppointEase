8471bdb69574d7b8c8ac8be635e8c999a3f4d4ac
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-09 23:22:15 +0530

    Modification

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -1,8 +1,9 @@
 <nav class="navbar">
     <div class="logo">Appoint<span>System</span></div>
    
-    <img src="../pngImages/menu.png" alt="Menu" class="lines">
-    <ul class="menu">
+    <img src="images/menu.png" alt="Menu" class="lines" id="menuIcon">
+   
+    <ul class="menu" id="menu">
         <li><a href="index.php">Home</a></li>
         <li><a href="Pages/services.php">Services</a></li>
         <li><a href="Pages/doctors.php">Doctors</a></li>
