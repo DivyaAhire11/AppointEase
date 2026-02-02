8bad95207257b12a38258c3c72865c947e927fc6
DivyaAhire11 <divyaahire121@gmail.com>
2026-02-02 21:18:58 +0530

    Database Connection

--- config/db.php
+++ config/db.php
@@ -1,15 +1,19 @@
 <?php
-// PostgreSQL Database Connection
-$host = "localhost";
-$port = 5432;
-$database = "hospital";
-$user = "postgres";
-$password = "tybcs";
 
-$conn = pg_connect("host=$host port=$port dbname=$database user=$user password=$password");
+$DB_HOST = 'localhost';
+$DB_PORT = 5432;
+$DB_NAME = 'hospital';
+$DB_USER = 'postgres';
+$DB_PASSWORD = 'tybcs';
 
+//database connection
+$conn = pg_connect(
+    "host =" . $DB_HOST . " port = " . $DB_PORT . " dbname=" . $DB_NAME .
+        " user=" . $DB_USER .
+        " password=" . $DB_PASSWORD
+);
+
+// Check connection
 if (!$conn) {
-   die("Error: Database connection failed - ");
+    die("Database connection failed. Please try again later");
 }
-
-?>
